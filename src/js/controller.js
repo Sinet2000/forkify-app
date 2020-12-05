35021619318990c60bb00d522baf58f0b7bcf498
N.Nikitins <nikita.nikitins@entrypoint.lv>
2020-12-05 18:10:27 +0200

    dividing app on MVC pattern

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,9 @@
+import * as model from './model';
+import recipeView from './recipes/recipeView.js';
+
+import 'regenerator-runtime/runtime';
+import 'core-js/stable';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -8,6 +14,30 @@ const timeout = function (s) {
   });
 };
 
+//API for this app:
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+
+    recipeView.renderSpinner();
+
+    // !) Loading recipe
+    await model.loadRecipe(id);
+    const recipe = model.state.recipe;
+
+    //2) rendering recipe
+    recipeView.render(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+['hashchange', 'load'].forEach(ev =>
+  window.addEventListener(ev, controlRecipes)
+);
